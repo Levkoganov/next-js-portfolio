264ebd224ee0a20e0accf31ab9eb37b2045ecc47
LevKoganov <levk@playsmart.co.il>
2024-01-30 12:42:28 +0200

    Geometries generate

--- app/components/Geometries.tsx
+++ app/components/Geometries.tsx
@@ -5,14 +5,40 @@ import Geometry from "./Geometry";
 const Geometries = () => {
   const geometries = [
     {
-      // position: [0, 0, 0],
       position: new THREE.Vector3(0, 0, 0),
       rate: 0.3,
       geometry: new THREE.IcosahedronGeometry(3), // Gem
     },
+    {
+      position: new THREE.Vector3(1, -0.75, 4),
+      rate: 0.4,
+      geometry: new THREE.CapsuleGeometry(0.5, 1.6, 2, 32), // Pill
+    },
+    {
+      position: new THREE.Vector3(-1.4, 2, -4),
+      rate: 0.6,
+      geometry: new THREE.DodecahedronGeometry(1.5), // Ball
+    },
+    {
+      position: new THREE.Vector3(-0.8, -0.75, 5),
+      rate: 0.5,
+      geometry: new THREE.TorusGeometry(0.6, 0.25, 16, 32), // Donut
+    },
+    {
+      position: new THREE.Vector3(1.6, 1.6, -4),
+      rate: 0.7,
+      geometry: new THREE.OctahedronGeometry(1.5), // Diamond
+    },
   ];
 
-  const materials = [new THREE.MeshNormalMaterial()];
+  const materials = [
+    new THREE.MeshNormalMaterial(),
+    new THREE.MeshStandardMaterial({ color: 0xd674ba, roughness: 0.3, metalness: 0.5 }), // pink
+    new THREE.MeshStandardMaterial({ color: 0x417efd, roughness: 0.3, metalness: 0.5 }), // blue
+    new THREE.MeshStandardMaterial({ color: 0x2bc8c8, roughness: 0.1, metalness: 0.5 }), // paleblue
+    new THREE.MeshStandardMaterial({ color: 0x8ee787, roughness: 0.3, metalness: 0.5 }), // green
+    // new THREE.MeshStandardMaterial({ color: 0x211b32, roughness: 0.5, metalness: 0.5 }), // red
+  ];
 
   return geometries.map(({ position, rate, geometry }) => (
     <Geometry
