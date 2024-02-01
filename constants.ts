916db72258c9fee4448d244c64419d43b6bb94d3
LevKoganov <levk@playsmart.co.il>
2024-02-01 12:29:36 +0200

    new costant var

--- constants.ts
+++ constants.ts
@@ -1,5 +1,6 @@
 export const firstName = "Lev";
-export const lastName = "koganov";
+export const lastName = "Koganov";
 export const tagLine = "creative software developer";
 export const textDescription = `I've started coding in 2020. Since then i been building, scaling, developing and desining software for some pretty cool companies. I also teach people to paint online
 (incase you've got an empty canvas layin' around 🎨). Let's connect!`;
+export const navBarItems = ["About", "Project", "Experience", "Contact"];
