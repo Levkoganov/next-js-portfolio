e38ab4fbac98f41e0ecef4021f05aff0447bc4af
LevKoganov <levk@playsmart.co.il>
2024-02-01 12:30:37 +0200

    header section

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { GoDownload } from "react-icons/go";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header>
-      <nav className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </nav>
+    <header className="sticky top-0 z-50 h-20 px-6 mx-auto flex w-full items-center justify-between">
+      <div className="flex gap-6">
+        <Link
+          href={"https://www.linkedin.com/in/lev-koganov-b4a045211/"}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaLinkedin
+            className="hover:text-yellow-500 transition duration-300 ease"
+            size={"22px"}
+          />
+        </Link>
+
+        <Link
+          href={"https://github.com/Levkoganov"}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaGithub
+            className="hover:text-yellow-500 transition duration-300 ease"
+            size={"22px"}
+          />
+        </Link>
+      </div>
+
+      <button className="btn btn-outline btn-warning">
+        My Resume
+        <GoDownload size={"18px"} />
+      </button>
     </header>
   );
 };
