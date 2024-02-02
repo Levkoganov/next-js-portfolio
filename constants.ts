4031c1cb3812be33e9ce84cfdf811d5925c1fc6b
LevKoganov <levk@playsmart.co.il>
2024-02-02 13:54:49 +0200

    tech constant

--- constants.ts
+++ constants.ts
@@ -4,3 +4,29 @@ export const tagLine = "creative software developer";
 export const textDescription = `I've started coding in 2020. Since then i been building, scaling, developing and desining software for some pretty cool companies. I also teach people to paint online
 (incase you've got an empty canvas layin' around 🎨). Let's connect!`;
 export const navBarItems = ["About", "Project", "Experience", "Contact"];
+export const coreTech = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Node.JS",
+  "HTML",
+  "CSS",
+  "Next.js",
+  "Redux",
+  "Express",
+  "MongoDB",
+  "GitHub",
+  "GitLab",
+  "Jira",
+  "Figma",
+];
+export const secondaryTech = [
+  "Python",
+  "Java",
+  "C#",
+  "AWS",
+  "Unity",
+  "Docker",
+  "MySQL",
+  "Linux",
+];
