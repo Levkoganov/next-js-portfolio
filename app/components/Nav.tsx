c55076e73f236068d0d170c4d647044e8021725a
LevKoganov <levk@playsmart.co.il>
2024-02-02 15:50:35 +0200

    [BUGFIX]: navbar & hover effect

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="bg-slate-950">
-      <nav className="sticky top-0 left-0 z-20 flex flex-col items-center ">
+    <div className="bg-slate-950 w-20">
+      <nav className="sticky top-0 left-0 z-20 flex flex-col items-center">
         <span className="flex justify-center items-center font-black h-20 w-full text-2xl tracking-tighter bg-slate-800">
           L<span className="text-yellow-500 font-black">.</span>K
         </span>
@@ -14,7 +14,8 @@ const Nav = () => {
             key={page}
             href={`#${page.toLowerCase()}`}
             style={{ writingMode: "vertical-lr" }}
-            className="px-6 py-6"
+            className="flex w-full items-center	py-6 opacity-50 text-lg tracking-wide
+            hover:opacity-100 hover:bg-slate-900 hover:border-r border-yellow-500"
           >
             {page}
           </Link>
