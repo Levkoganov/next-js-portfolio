3ab49386ac997986f730b39bcde450c00294f8ac
LevKoganov <levk@playsmart.co.il>
2024-02-04 12:59:04 +0200

    portfolio page

---
+++ app/projects/Card.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import style from "./style/card.module.css";
+
+interface IStack {
+  icons: any[];
+  names: string[];
+}
+
+interface IProps {
+  header: string;
+  description: string;
+  stack: IStack;
+  link: string;
+}
+
+const Card = ({ header, description, stack, link }: IProps) => {
+  const { icons, names } = stack;
+
+  return (
+    <div
+      className={`${style.cardshadow} card card-side rounded-none border-x border-black bg-slate-800  hover:-translate-y-3 hover:-translate-x-3 transition duration-200 ease-in-out`}
+    >
+      <div className="card-body gap-8">
+        <h2
+          className="card-title justify-center bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500
+           bg-clip-text text-3xl font-bold  text-transparent"
+        >
+          {header}
+        </h2>
+        <p>{description}</p>
+        <div className="card-actions justify-center column">
+          <div className="grid grid-rows-2">
+            <div className="grid grid-flow-col auto-cols-fr justify-items-center gap-4">
+              {icons.map((item, idx) => (
+                <Image
+                  key={idx}
+                  src={item}
+                  alt="icon"
+                  style={{ width: "28px", height: "28px" }}
+                />
+              ))}
+            </div>
+
+            <div className="grid grid-flow-col auto-cols-fr items-center justify-items-center gap-4">
+              {names.map((item, idx) => (
+                <p key={idx} className="text-xs">
+                  {item}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="card-actions justify-center">
+          <Link
+            className="btn btn-outline btn-wide bg-slate-900"
+            href={link}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            GitHub <FaGithub size={"22px"} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- app/projects/Projects.tsx
+++ app/projects/Projects.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Bounded from "../components/Bounded";
 import HorizontalRow from "../components/HorizontalRow";
+import Card from "./Card";
+import { projectsInfo } from "@/constants";
 
 const Projects = () => {
   return (
     <Bounded>
-      <div id="projects" className="min-h-[100vh]">
-        <HorizontalRow pageName={"projects"} isRowReverse={true} color={2} />
+      <div id="project" className="min-h-[100vh]">
+        <HorizontalRow pageName={"projects"} isRowReverse={true} />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {projectsInfo.map(({ projectId, header, description, stack, link }) => (
+            <Card
+              key={projectId}
+              header={header}
+              description={description}
+              stack={stack}
+              link={link}
+            />
+          ))}
+        </div>
       </div>
     </Bounded>
   );
