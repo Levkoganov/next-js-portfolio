48db4e37a1cf99497bdffc4a84062094996211cf
LevKoganov <levk@playsmart.co.il>
2024-02-05 14:46:52 +0200

    collapse resume

--- app/experience/Sectors.tsx
+++ app/experience/Sectors.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 interface IProps {
+  id: number;
   companyName: string;
   workDuration: string;
   title: string;
@@ -13,6 +14,7 @@ interface IProps {
 }
 
 const Sectors = ({
+  id,
   companyName,
   workDuration,
   title,
@@ -22,47 +24,55 @@ const Sectors = ({
   technologies,
 }: IProps) => {
   return (
-    <div className="flex flex-col gap-4 mb-8 pb-8 border-b-2 border-slate-700">
-      <div className="flex justify-between text-3xl font-bold">
-        <h1>{companyName}</h1>
-        <h1>{workDuration}</h1>
-      </div>
+    <div className="cardshadow join join-vertical w-full bg-slate-800 py-2 mb-4 border-x border-slate-950">
+      <div className="collapse collapse-arrow join-item">
+        <input type="radio" name="my-accordion-4" defaultChecked={id === 1} />
+        <div className="collapse-title text-xl font-medium">
+          <div className="flex justify-between text-3xl font-bold flex-col text-slate-300">
+            <div className="flex justify-between">
+              <h1>{companyName}</h1>
+              <h1>{workDuration}</h1>
+            </div>
 
-      <div
-        className="flex justify-between bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500
-           bg-clip-text text-2xl font-bold  text-transparent"
-      >
-        <h1>{title}</h1>
-        <h1>{location}</h1>
-      </div>
-
-      <div className="md:w-2/5">
-        <p>{description}</p>
-      </div>
-
-      <div className="flex flex-wrap gap-3">
-        {technologies.map((item) => (
-          <span
-            className="text-center font-bold text-base text bg-slate-800 px-4 py-1 rounded-xl border select-none"
-            key={item}
-          >
-            {item}
-          </span>
-        ))}
-      </div>
-
-      {companywebsite && (
-        <div>
-          <Link
-            className="btn btn-outline btn-success"
-            href={companywebsite}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Live Website <FaExternalLinkAlt />
-          </Link>
+            <div
+              className="flex gap-2 flex-row justify-between bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500
+            bg-clip-text text-lg font-semibold text-transparent"
+            >
+              <h1>{title}</h1>
+              <h1>{location}</h1>
+            </div>
+          </div>
         </div>
-      )}
+        <div className="collapse-content flex flex-col gap-4">
+          <div className="md:w-2/5">
+            <p>{description}</p>
+          </div>
+
+          <div className="flex flex-wrap gap-3">
+            {technologies.map((item) => (
+              <span
+                className="text-center font-bold text-base text bg-slate-900 px-4 py-1 rounded-xl border-2 border-slate-500 select-none"
+                key={item}
+              >
+                {item}
+              </span>
+            ))}
+          </div>
+
+          {companywebsite && (
+            <div>
+              <Link
+                className="btn btn-outline btn-success"
+                href={companywebsite}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Live Website <FaExternalLinkAlt />
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
