8e6034a4f2029bef7b84df5e4701bb9052be687e
LevKoganov <levk@playsmart.co.il>
2024-02-05 11:31:19 +0200

    [BUGFIX]: Card layout

--- app/projects/Card.tsx
+++ app/projects/Card.tsx
@@ -21,17 +21,17 @@ const Card = ({ header, description, stack, link }: IProps) => {
 
   return (
     <div
-      className={`${style.cardshadow} card card-side rounded-none border-x border-black bg-slate-800  hover:-translate-y-3 hover:-translate-x-3 transition duration-200 ease-in-out`}
+      className={`${style.cardshadow} card rounded-none border-x border-black bg-slate-800  hover:-translate-y-3 hover:-translate-x-3 transition duration-200 ease-in-out`}
     >
-      <div className="card-body gap-8">
+      <div className="card-body items-center gap-8">
         <h2
-          className="card-title justify-center bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500
+          className="card-title bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500
            bg-clip-text text-3xl font-bold  text-transparent"
         >
           {header}
         </h2>
         <p>{description}</p>
-        <div className="card-actions justify-center column">
+        <div className="card-actions column">
           <div className="grid grid-rows-2">
             <div className="grid grid-flow-col auto-cols-fr justify-items-center gap-4">
               {icons.map((item, idx) => (
@@ -53,7 +53,8 @@ const Card = ({ header, description, stack, link }: IProps) => {
             </div>
           </div>
         </div>
-        <div className="card-actions justify-center">
+
+        <div className="card-actions">
           <Link
             className="btn btn-outline btn-wide bg-slate-900"
             href={link}
