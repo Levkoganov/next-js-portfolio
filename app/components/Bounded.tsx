fa84822221bd3e319327a4f804db0fac1a27a60f
LevKoganov <levk@playsmart.co.il>
2024-02-06 12:45:17 +0200

    center div

--- app/components/Bounded.tsx
+++ app/components/Bounded.tsx
@@ -5,20 +5,22 @@ type BoundedProps = {
   as?: React.ElementType;
   className?: string;
   children?: React.ReactNode;
+  pageid?: string;
 };
 
 const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
   ({ as: Comp = "section", className, children, ...restProps }, ref) => {
     return (
-      // <Comp
-      //   ref={ref}
-      //   className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
-      //   {...restProps}
-      // >
-      <Comp ref={ref} className={clsx("px-4 md:px-6", className)} {...restProps}>
-        {/* <div className="m-auto w-4/5 ">{children}</div> */}
-        {/* <div className="m-auto w-full max-w-screen-2xl">{children}</div> */}
-        <div className="m-auto w-full max-w-screen-2xl">{children}</div>
+      <Comp
+        ref={ref}
+        className={clsx("relative px-4 md:px-6 border-b-2 border-slate-950", className)}
+        {...restProps}
+      >
+        <div className="m-auto w-full max-w-screen-2xl xl:py-0 py-10">
+          <div id={restProps.pageid} className="flex items-center min-h-[100vh]">
+            {children}
+          </div>
+        </div>
       </Comp>
     );
   }
