86921fb7f371040b4035b4ed8faa29f2fbdc7d25
LevKoganov <levk@playsmart.co.il>
2024-02-06 12:45:26 +0200

    style fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mb-4 px-4 text-center text-gray-400">
+    <footer className="py-2 px-4 text-center text-gray-400 bottom-0 absolute">
       <small className="text-s">© 2024 Lev Koganov. All rights reserved.</small>
       <p className="text-xs">
         <span className="font-semibold">
