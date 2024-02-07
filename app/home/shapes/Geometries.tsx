774922e90c82200bf7dbe2590ea08d23ccd19894
LevKoganov <levk@playsmart.co.il>
2024-02-07 15:30:39 +0200

    remove shapes

--- app/home/shapes/Geometries.tsx
+++ app/home/shapes/Geometries.tsx
@@ -5,29 +5,9 @@ import Geometry from "./Geometry";
 const Geometries = () => {
   const geometries = [
     {
-      position: new THREE.Vector3(0, 0, 0),
-      rate: 0.3,
-      geometry: new THREE.IcosahedronGeometry(3), // Gem
-    },
-    {
-      position: new THREE.Vector3(1, -0.75, 4),
+      position: new THREE.Vector3(0, 0.1, 0),
       rate: 0.4,
-      geometry: new THREE.CapsuleGeometry(0.5, 1.6, 2, 32), // Pill
-    },
-    {
-      position: new THREE.Vector3(-1.4, 2, -4),
-      rate: 0.6,
-      geometry: new THREE.DodecahedronGeometry(1.5), // Ball
-    },
-    {
-      position: new THREE.Vector3(-0.8, -0.75, 5),
-      rate: 0.5,
-      geometry: new THREE.TorusGeometry(0.6, 0.25, 16, 32), // Donut
-    },
-    {
-      position: new THREE.Vector3(1.6, 1.6, -4),
-      rate: 0.7,
-      geometry: new THREE.OctahedronGeometry(1.5), // Diamond
+      geometry: new THREE.IcosahedronGeometry(3), // Gem
     },
   ];
 
