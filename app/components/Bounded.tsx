df63e26897249ef03d52b95176c8969a2f35dc20
LevKoganov <levk@playsmart.co.il>
2024-02-11 20:10:17 +0200

    Removing div

--- app/components/Bounded.tsx
+++ app/components/Bounded.tsx
@@ -16,11 +16,7 @@ const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
         className={clsx("relative px-4 md:px-6 border-b-2 border-slate-900", className)}
         {...restProps}
       >
-        <div className="m-auto w-full max-w-screen-2xl">
-          <div id={restProps.pageid} className="flex items-center min-h-[100vh] py-24">
-            {children}
-          </div>
-        </div>
+        <div className="m-auto w-full max-w-screen-2xl">{children}</div>
       </Comp>
     );
   }
